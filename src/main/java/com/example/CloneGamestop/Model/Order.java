c8352e071835516d4ff07cbc27ca2690f8c95241
Mike <109431533+1111334@users.noreply.github.com>
2023-12-30 01:48:06 +0100

    aggiunto annotazioni classi controller

--- src/main/java/com/example/CloneGamestop/Model/Order.java
+++ src/main/java/com/example/CloneGamestop/Model/Order.java
@@ -1,4 +1,9 @@
 package com.example.CloneGamestop.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "order")
 public class Order {
 }
--- src/main/java/com/example/CloneGamestop/Model/OrderDetails.java
+++ src/main/java/com/example/CloneGamestop/Model/OrderDetails.java
@@ -1,4 +1,10 @@
 package com.example.CloneGamestop.Model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orderDetails")
 public class OrderDetails {
 }
