f410f0ef5a25d067cde266d50f313260bd20125f
Mike <109431533+1111334@users.noreply.github.com>
2023-12-30 01:36:17 +0100

    primo commit

--- src/main/java/com/example/CloneGamestop/Service/UserService.java
+++ src/main/java/com/example/CloneGamestop/Service/UserService.java
@@ -1,4 +1,13 @@
 package com.example.CloneGamestop.Service;
 
+import com.example.CloneGamestop.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
 }
