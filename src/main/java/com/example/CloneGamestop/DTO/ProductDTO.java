2bfaec5a2d98551c8e0f8bc122aa429cd01e9f43
Mike <109431533+1111334@users.noreply.github.com>
2024-01-07 16:52:22 +0100

    aggiunto di DTO per l'entità User e Product

---
+++ src/main/java/com/example/CloneGamestop/DTO/ProductDTO.java
@@ -0,0 +1,47 @@
+package com.example.CloneGamestop.DTO;
+
+import com.example.CloneGamestop.Model.Product;
+import com.example.CloneGamestop.Model.User;
+
+public class ProductDTO {
+
+    private String name;
+    private String description;
+    private int price;
+
+    public ProductDTO() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public static ProductDTO fromProduct(Product product) {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setName(product.getName());
+        productDTO.setDescription(product.getDescription());
+        productDTO.setPrice(product.getPrice());
+        return productDTO;
+    }
+}
