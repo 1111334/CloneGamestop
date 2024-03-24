915d111fc8519957a331d6af16b993c4aa03e7fa
Mike <109431533+1111334@users.noreply.github.com>
2024-03-24 13:15:55 +0100

    problema bug application-properties risolto rimettendo dipendenze del progetto salvato in precendenza pulito la cache e riavviato sistema

--- src/main/java/com/example/CloneGamestop/CloneGamestopApplication.java
+++ src/main/java/com/example/CloneGamestop/CloneGamestopApplication.java
@@ -3,10 +3,9 @@ package com.example.CloneGamestop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.example.CloneGamestop")
+//@ComponentScan("com.example.CloneGamestop")
 public class CloneGamestopApplication {
 
 	public static void main(String[] args) {
